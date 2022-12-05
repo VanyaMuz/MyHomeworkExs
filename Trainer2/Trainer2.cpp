e5b0a4607c536d17939466e1cfb892c4a5c44d6d
VanyaMuz <i.muzykantau@gmail.com>
2022-12-05 23:21:09 +0300

    new prog

---
+++ Trainer2/Trainer2.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+
+
+
+using namespace std;
+
+class MyString
+{
+private:
+	char* buffer;
+
+public:
+
+	MyString(const char* initString)
+	{
+		buffer = new char[strlen(initString)+1] {0};
+		
+		cout << "buffer " << (double*)buffer << " strlen " << &initString << endl;
+		
+		strcpy_s(buffer, strlen(initString)+1, initString);
+
+		cout << "buffer " << &buffer[0] << " strlen " << &initString << endl;
+	}
+	~MyString()
+	{
+		delete[] buffer;
+	}
+	int GetLength()
+	{
+		return strlen(buffer);
+	}
+	const char* GetString()
+	{
+		return buffer;
+	}
+};
+
+int main()
+{
+	MyString sayHello("Hello Man!");
+	cout << sayHello.GetString() << endl;
+	cout << sayHello.GetLength() << endl;
+
+
+	system("pause");
+	return 0;
+}
